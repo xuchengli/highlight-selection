a0709a2c7069016e363c5acb025343b81e74ec07
xuchengli <lixucheng@aliyun.com>
2023-05-16 19:29:15 +0800

    count highlight selection

--- extension.js
+++ extension.js
@@ -1,30 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const { window, workspace, StatusBarAlignment } = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function escapeRegExp(str) {
+	return str.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getAlignmentValue(config) {
+	const alignment = config.get('alignment');
+	return alignment === StatusBarAlignment[StatusBarAlignment.Left].toLowerCase()
+        ? StatusBarAlignment.Left
+        : StatusBarAlignment.Right;
+}
+
+function getPriorityValue(config) {
+	return Number(config.get('priority'));
+}
+
+function getCaseSensitiveValue(config) {
+	return config.get('caseSensitive') || false;
+}
+
+function getWholeWordMatchingValue(config) {
+	return config.get('wholeWordMatching') || false;
+}
+
+function getWordSeparatorsValue(config) {
+	let wordSeparators = config.get('wordSeparators');
+	if (wordSeparators) return wordSeparators;
+
+	wordSeparators = workspace.getConfiguration('editor').get('wordSeparators');
+	return wordSeparators ? wordSeparators : '';
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	const config = workspace.getConfiguration('highlight-selection');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "highlight-selection" is now active!');
+	const alignment = getAlignmentValue(config);
+  const priority = getPriorityValue(config);
+  const caseSensitive = getCaseSensitiveValue(config);
+  const wholeWordMatching = getWholeWordMatchingValue(config);
+  const wordSeparators = getWordSeparatorsValue(config);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('highlight-selection.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	const msg = window.createStatusBarItem(alignment, priority);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from highlight-selection!');
-	});
+	let matchFlags = 'gi';
+	if (caseSensitive) {
+		matchFlags = 'g';
+	}
 
-	context.subscriptions.push(disposable);
+	const decorationTypes = {};
+	let match;
+	let counter = 0;
+
+	window.onDidChangeTextEditorSelection(() => {
+		counter = 0;
+		const editor = window.activeTextEditor;
+
+		if (editor) {
+			let text = editor.document.getText(editor.selection);
+
+			if (text.length >= 1 && text.indexOf('\n') === -1) {
+				text = escapeRegExp(text);
+
+				if (wholeWordMatching) {
+					const boundaryRegex = wordSeparators ? `[${escapeRegExp(wordSeparators)}]|\\s` : '\\b';
+          text = `(?<=^|${boundaryRegex})${text}(?=$|${boundaryRegex})`;
+				}
+
+				const pattern = new RegExp(text, matchFlags);
+
+				while (match = pattern.exec(editor.document.getText())) {
+					var startPos = editor.document.positionAt(match.index);
+					var endPos = editor.document.positionAt(match.index + match[0].length);
+
+					console.log(match[0]);
+					console.log(startPos, endPos);
+
+					counter++;
+				}
+
+				if (counter) {
+					console.log('highlight count: ', counter);
+				} else if (msg) {
+					msg.hide();
+				}
+			} else if (msg) {
+				msg.hide();
+			}
+		} else if (msg) {
+			msg.hide();
+		}
+	}, null, context.subscriptions);
 }
 
 // This method is called when your extension is deactivated
